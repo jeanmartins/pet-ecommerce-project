7eb586414180ce8c3be5d452b8d47435707df546
jean_martins <jean.carlos.martins00@gmail.com>
2023-06-01 19:00:51 -0300

    change app name

--- pet-ecommerce/Pet.Commerce.Api/Program.cs
+++ pet-ecommerce/Pet.Commerce.Api/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen( c=> {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "apiagenda", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Pet.Commerce.API", Version = "v1" });
 
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
     {
