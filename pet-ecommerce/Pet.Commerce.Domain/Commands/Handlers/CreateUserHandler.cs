342a92adb3a3a2025a90a785616864ff12250637
jean_martins <jean.carlos.martins00@gmail.com>
2023-06-02 01:03:52 -0300

    update create user handler

--- pet-ecommerce/Pet.Commerce.Domain/Commands/Handlers/CreateUserHandler.cs
+++ pet-ecommerce/Pet.Commerce.Domain/Commands/Handlers/CreateUserHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Microsoft.IdentityModel.Tokens;
 using Pet.Commerce.Domain.Commands.Handlers.Requests;
 using Pet.Commerce.Domain.Commands.Responses;
 using Pet.Commerce.Domain.IRepositories;
@@ -15,13 +16,25 @@ namespace Pet.Commerce.Domain.Commands.Handlers
         }
         public Task<CreateUserResponse> Handle(CreateUserCommand request, CancellationToken cancellationToken)
         {
-            if(_userRepository.GetUserByEmail(request.Email) != null)
+            try
             {
-                return Task.FromResult(new CreateUserResponse { ErrorMessage = "Usuário já cadastrado no sistema" });
+                if (_userRepository.GetUserByEmail(request.Email) != null)
+                {
+                    return Task.FromResult(new CreateUserResponse { ErrorMessage = "Usuário já cadastrado no sistema" });
+                }
+                if (!string.IsNullOrEmpty(request.Email) && !string.IsNullOrEmpty(request.Endereco) && !string.IsNullOrEmpty(request.Nome) && !string.IsNullOrEmpty(request.Senha))
+                {
+                    Usuario usu = new Usuario { Administrador = request.Administrador, Email = request.Email, Endereco = request.Endereco, Login = Guid.NewGuid().ToString("N"), Nome = request.Nome, Senha = request.Senha };
+                    _userRepository.Insert(usu);
+                    return Task.FromResult(new CreateUserResponse { Nome = usu.Nome, Endereco = usu.Endereco, Email = usu.Email });
+                }
+                return Task.FromResult(new CreateUserResponse { ErrorMessage = "Ocorreu um erro ao cadastrar usuário" });
             }
-            Usuario usu = new Usuario { Administrador = request.Administrador, Email = request.Email, Endereco=request.Endereco, Login=Guid.NewGuid().ToString("N"), Nome=request.Nome, Senha = request.Senha };
-            _userRepository.Insert(usu);
-            return Task.FromResult(new CreateUserResponse { Nome = usu.Nome, Endereco = usu.Endereco, Email = usu.Email });
+            catch (Exception ex)
+            {
+                return Task.FromResult(new CreateUserResponse { ErrorMessage = "Ocorreu um erro ao cadastrar usuário" });
+
+            }
         }
     }
 }
